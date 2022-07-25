6f05f1b605b0cdce795c23d523d456a3787816ea
Zhuangkang <2247830091@qq.com>
2022-07-25 12:57:55 +0800

    测试Eclipse

--- src/com/zhuang/dynamic_list/Test.java
+++ src/com/zhuang/dynamic_list/Test.java
@@ -4,7 +4,7 @@ package com.zhuang.dynamic_list;
 public class Test {
 	
 	public static void main(String[] args) {
-		//测试修改
+		//测试Eclipse
 		ArrayList<Person> arrayList = new ArrayList<>();
 		arrayList.add(new Person("ZK123",12));
 		arrayList.add(null);
