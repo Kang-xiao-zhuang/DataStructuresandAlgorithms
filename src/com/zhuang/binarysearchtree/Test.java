688c06603afa83fe7cc1ac41bcadf473b4de5cac
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-22 10:45:26 +0800

    树的测试类

--- src/com/zhuang/binarysearchtree/Test.java
+++ src/com/zhuang/binarysearchtree/Test.java
@@ -9,10 +9,27 @@ import com.zhuang.binarysearchtree.printer.BinaryTrees;
 public class Test {
 
 	public static void main(String[] args) {
-		 test1();
+		// test1();
 		// test2();
 		// test3();
 		// test4();
+		// test5();
+		// test6();
+		// test7();
+		// test8();
+		test9();
+	}
+
+	private static class PersonComparator implements Comparator<Person> {
+		public int compare(Person e1, Person e2) {
+			return e1.getAge() - e2.getAge();
+		}
+	}
+
+	private static class PersonComparator2 implements Comparator<Person> {
+		public int compare(Person e1, Person e2) {
+			return e2.getAge() - e1.getAge();
+		}
 	}
 
 	public static void test1() {
@@ -55,9 +72,9 @@ public class Test {
 
 		String str = BinaryTrees.printString(bst);
 		str += "\n";
-		// Files.writeToFile("F:/1.txt", str, true);
+		Files.writeToFile("F:/1.txt", str, true);
 
-		BinaryTrees.println(bst);
+		// BinaryTrees.println(bst);
 	}
 
 	public static void test4() {
@@ -91,5 +108,145 @@ public class Test {
 				return null;
 			}
 		});
+
+		// test3();
+
+		/*
+		 * Java的匿名类，类似于iOS中的Block、JS中的闭包（function）
+		 */
+
+//		BinarySearchTree<Person> bst1 = new BinarySearchTree<>(new Comparator<Person>() {
+//			@Override
+//			public int compare(Person o1, Person o2) {
+//				return 0;
+//			}
+//		});
+//		bst1.add(new Person(12));
+//		bst1.add(new Person(15));
+//		
+//		BinarySearchTree<Person> bst2 = new BinarySearchTree<>(new PersonComparator());
+//		bst2.add(new Person(12));
+//		bst2.add(new Person(15));
+//
+
+//		BinarySearchTree<Car> bst4 = new BinarySearchTree<>(new Car);
+//		
+//		
+//		java.util.Comparator<Integer> iComparator;
+	}
+
+	public static void test5() {
+		BinarySearchTree<Person> bst = new BinarySearchTree<>();
+		bst.add(new Person(10, "jack"));
+		bst.add(new Person(12, "rose"));
+		bst.add(new Person(6, "jim"));
+
+		bst.add(new Person(10, "michael"));
+
+		BinaryTrees.println(bst);
+	}
+
+	public static void test6() {
+		Integer data[] = new Integer[] { 7, 4, 9, 2, 5 };
+
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		for (int i = 0; i < data.length; i++) {
+			bst.add(data[i]);
+		}
+
+//		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+//		for (int i = 0; i < 10; i++) {
+//			bst.add((int)(Math.random() * 100));
+//		}
+		BinaryTrees.println(bst);
+		System.out.println(bst.isComplete());
+
+		// bst.levelOrderTraversal();
+
+		/*
+		 * 7 4 9 2 5
+		 */
+
+//		bst.levelOrder(new Visitor<Integer>() {
+//			public void visit(Integer element) {
+//				System.out.print("_" + element + "_ ");
+//			}
+//		});
+
+//		bst.inorder(new Visitor<Integer>() {
+//			public void visit(Integer element) {
+//				System.out.print("_" + (element + 3) + "_ ");
+//			}
+//		});
+
+		// System.out.println(bst.height());
+	}
+
+	public static void test7() {
+		Integer data[] = new Integer[] { 7, 4, 9, 2, 5, 8, 11, 3, 12, 1 };
+
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		for (int i = 0; i < data.length; i++) {
+			bst.add(data[i]);
+		}
+
+		BinaryTrees.println(bst);
+
+		bst.remove(7);
+
+		BinaryTrees.println(bst);
+	}
+
+	public static void test8() {
+		Integer data[] = new Integer[] { 7, 4, 9, 2, 1 };
+
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		for (int i = 0; i < data.length; i++) {
+			bst.add(data[i]);
+		}
+		BinaryTrees.println(bst);
+		System.out.println(bst.isComplete());
+	}
+
+	public static void test9() {
+		Integer data[] = new Integer[] { 7, 4, 9, 2, 1 };
+
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		for (int i = 0; i < data.length; i++) {
+			bst.add(data[i]);
+		}
+		BinaryTrees.println(bst);
+
+		bst.preorder(new Visitor<Integer>() {
+			public boolean visit(Integer element) {
+				System.out.print(element + " ");
+				return element == 2 ? true : false;
+			}
+		});
+		System.out.println();
+
+		bst.inorder(new Visitor<Integer>() {
+			public boolean visit(Integer element) {
+				System.out.print(element + " ");
+				return element == 4 ? true : false;
+			}
+		});
+		System.out.println();
+
+		bst.postorder(new Visitor<Integer>() {
+			public boolean visit(Integer element) {
+				System.out.print(element + " ");
+				return element == 4 ? true : false;
+			}
+		});
+		System.out.println();
+
+		bst.levelOrder(new Visitor<Integer>() {
+			public boolean visit(Integer element) {
+				System.out.print(element + " ");
+				return element == 9 ? true : false;
+			}
+		});
+		System.out.println();
 	}
 }
