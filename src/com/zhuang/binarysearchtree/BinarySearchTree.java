ac4ee4b734a0c7fb4cdb847036be46b3098da57d
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-22 10:33:41 +0800

    添加remove方法

--- src/com/zhuang/binarysearchtree/BinarySearchTree.java
+++ src/com/zhuang/binarysearchtree/BinarySearchTree.java
@@ -76,6 +76,74 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
 		size++;
 	}
 
+	public void remove(E element) {
+		remove(node(element));
+	}
+
+	public boolean contains(E element) {
+		return node(element) != null;
+	}
+
+	private void remove(Node<E> node) {
+		if (node == null) {
+			return;
+		}
+		size--;
+		// 度为2的节点
+		if (node.hasTwoChildren()) {
+			// 找到后继节点
+			com.zhuang.binarysearchtree.Node<E> successor = successor(node);
+			// 后继节点的值覆盖度为2的节点
+			node.element = successor.element;
+			// 删除后继节点
+			node = successor;
+		}
+		// 删除node节点（node的度必然是1或者0）
+		Node<E> deleteNode = node.left != null ? node.left : node.right;
+		// node是度为1的节点
+		if (deleteNode != null) {
+			// 更改parent
+			deleteNode.parent = node.parent;
+			// 更改parent的left，right的指向
+			if (node.parent == null) {
+				// node是度为1的节点并且是根节点
+				root = deleteNode;
+			} else if (node == node.parent.left) {
+				// 删除节点是父节点的左子节点
+				node.parent.left = deleteNode;
+			} else if (node == node.parent.right) {
+				// 删除节点是父节点的右子节点
+				node.parent.right = deleteNode;
+			}
+		} else if (node.parent == null) {
+			// node为叶子节点并且是根节点
+			root = null;
+		} else if (node.parent != null) {// node是叶子节点，但不是根节点
+			if (node == node.parent.left) {
+				node.parent.left = null;
+			} else if (node == node.parent.right) {
+				node.parent.right = null;
+			}
+		}
+
+	}
+
+	private Node<E> node(E element) {
+		Node<E> node = root;
+		while (node != null) {
+			int cmp = compare(element, node.element);
+			if (cmp == 0) {
+				return node;
+			}
+			if (cmp > 0) {
+				node = node.right;
+			} else { // cmp < 0
+				node = node.left;
+			}
+		}
+		return null;
+	}
+
 	/**
 	 * 前序遍历
 	 */
@@ -381,11 +449,5 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
 		}
 		return myNode.element + "_p(" + parentString + ")";
 	}
-
-	/*
-	 * public void remove(E element) { remove(node(element)); }
-	 * 
-	 * public boolean contains(E element) { return node(element) != null; }
-	 */
 
 }
