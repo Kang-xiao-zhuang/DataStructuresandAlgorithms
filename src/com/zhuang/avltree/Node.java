c21430e699057ec7a892096bdc192be79a95582a
Kang-xiao-zhuang <2247830091@qq.com>
2022-10-23 10:57:39 +0800

    Node节点

---
+++ src/com/zhuang/avltree/Node.java
@@ -0,0 +1,41 @@
+package com.zhuang.avltree;
+
+public class Node<E> {
+	E element;
+	Node<E> left;
+	Node<E> right;
+	Node<E> parent;
+
+	public Node(E element, Node<E> parent) {
+		this.element = element;
+		this.parent = parent;
+	}
+
+	public boolean isLeaf() {
+		return left == null && right == null;
+	}
+
+	public boolean hasTwoChildren() {
+		return left != null && right != null;
+	}
+
+	public boolean isLeftChild() {
+		return parent != null && this == parent.left;
+	}
+
+	public boolean isRightChild() {
+		return parent != null && this == parent.right;
+	}
+
+	public Node<E> sibling() {
+		if (isLeftChild()) {
+			return parent.right;
+		}
+
+		if (isRightChild()) {
+			return parent.left;
+		}
+
+		return null;
+	}
+}
